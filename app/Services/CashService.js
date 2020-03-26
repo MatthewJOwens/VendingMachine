26443c3b2d714edc9f60885ceadffec45ed21ae3
MatthewJOwens <mjowens@mail.com>
2020-03-26 15:00:41 -0600

    addCash() works properly

--- app/Services/CashService.js
+++ app/Services/CashService.js
@@ -3,6 +3,7 @@ import _store from '../store.js'
 class CashService {
   addCash() {
     _store.State.cash += 0.25
+    document.getElementById("money").innerText = '$' + _store.State.cash.toFixed(2)
   }
 
   constructor() {
